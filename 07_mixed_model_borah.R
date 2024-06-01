42873ea1c1764e2054efb55f83070ee87aa88332
carlibeisel <143646106+carlibeisel@users.noreply.github.com>
2024-06-01 09:24:30 -0600

    updated for annual prcp

--- 07_mixed_model_borah.R
+++ 07_mixed_model_borah.R
@@ -27,7 +27,7 @@ library(loo)
 
 # Import the data 
 
-rf <- read.csv('/Users/dbeisel/Desktop/DATA/Bridget/Drains_Lower_Boise_River/model_input/mixed_model_input.csv')
+rf <- read.csv('/Users/dbeisel/Desktop/DATA/Bridget/Drains_Lower_Boise_River/model_input/mixed_model_input_0531.csv')
 rf$lt <- log(rf$Sum_AF)
 
 ## MODEL FIT ####
