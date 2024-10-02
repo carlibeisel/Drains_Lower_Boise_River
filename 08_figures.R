dab15214898c3329d4fdcac8a847728eb18add39
carlibeisel <143646106+carlibeisel@users.noreply.github.com>
2024-10-02 12:14:30 -0600

    edit

--- 08_figures.R
+++ 08_figures.R
@@ -28,7 +28,7 @@ unscale <- function(x, orig){
 
 ## Import data ####
 rf <- read.csv('/Users/dbeisel/Desktop/DATA/Bridget/Drains_Lower_Boise_River/model_input/mixed_model_input_0912.csv')  
-arma_ng <- readRDS('/Users/dbeisel/Desktop/DATA/Bridget/Drains_Lower_Boise_River/model_output/arma_nogroup_mod2.Rdata')
+arma_ng <- readRDS('/Users/dbeisel/Desktop/DATA/Bridget/Drains_Lower_Boise_River/model_output/arma_nogroup_mod3.Rdata')
 
 # Check out model summary 
 
@@ -70,12 +70,12 @@ new = rf %>%
             scale_et = mean(scale_et),
             scale_irrig_prcp = mean(scale_irrig_prcp), 
             scale_irrig_temp = mean(scale_irrig_temp),
-            scale_DivFlow = mean(scale_DivFlow),
+            scale_DivFlow = mean(scale_DivFlow))
             scale_ubrb_prcp = mean(scale_ubrb_prcp),
             scale_pivot_perc = mean(scale_pivot_perc),
             scale_Carryover = mean(scale_Carryover),
-            scale_sw_wr = mean(scale_sw_wr),
-            scale_gw_wr = mean(scale_gw_wr)) 
+            #scale_sw_wr = mean(scale_sw_wr),
+            #scale_gw_wr = mean(scale_gw_wr)) 
 new$Name <- NA
 
 ## Step 2: generate predictions from model:
@@ -123,12 +123,12 @@ simdata = rf %>%
             scale_irrig_prcp = mean(scale_irrig_prcp),
             scale_irrig_temp = mean(scale_irrig_temp),
             scale_et = seq_range(scale_et, n=200),
-            scale_DivFlow = mean(scale_DivFlow),
+            scale_DivFlow = mean(scale_DivFlow))
             scale_ubrb_prcp = mean(scale_ubrb_prcp),
             scale_pivot_perc = mean(scale_pivot_perc),
             scale_Carryover = mean(scale_Carryover),
-            scale_sw_wr = mean(scale_sw_wr),
-            scale_gw_wr = mean(scale_gw_wr)) 
+            #scale_sw_wr = mean(scale_sw_wr),
+            #scale_gw_wr = mean(scale_gw_wr)) 
 simdata$Name <- NA
 epreddraws <-  add_epred_draws(arma_ng, 
                                newdata=simdata,
@@ -176,12 +176,12 @@ simdata = rf %>%
             scale_irrig_prcp = mean(scale_irrig_prcp),
             scale_irrig_temp = seq_range(scale_irrig_temp, n=200),
             scale_et = mean(scale_et),
-            scale_DivFlow = mean(scale_DivFlow),
+            scale_DivFlow = mean(scale_DivFlow))
             scale_ubrb_prcp = mean(scale_ubrb_prcp),
             scale_pivot_perc = mean(scale_pivot_perc),
             scale_Carryover = mean(scale_Carryover),
-            scale_sw_wr = mean(scale_sw_wr),
-            scale_gw_wr = mean(scale_gw_wr)) 
+            #scale_sw_wr = mean(scale_sw_wr),
+            #scale_gw_wr = mean(scale_gw_wr)) 
 simdata$Name <- NA
 
 epreddraws <-  add_epred_draws(arma_ng, 
@@ -233,9 +233,9 @@ simdata = rf %>%
             scale_DivFlow = mean(scale_DivFlow),
             scale_ubrb_prcp = mean(scale_ubrb_prcp),
             scale_pivot_perc = seq_range(scale_pivot_perc, n = 200),
-            scale_Carryover = mean(scale_Carryover),
-            scale_sw_wr = mean(scale_sw_wr),
-            scale_gw_wr = mean(scale_gw_wr)) 
+            scale_Carryover = mean(scale_Carryover))
+            #scale_sw_wr = mean(scale_sw_wr),
+            #scale_gw_wr = mean(scale_gw_wr)) 
 simdata$Name <- NA
 
 epreddraws <-  add_epred_draws(arma_ng, 
@@ -285,9 +285,9 @@ simdata = rf %>%
             scale_DivFlow = mean(scale_DivFlow),
             scale_ubrb_prcp = mean(scale_ubrb_prcp),
             scale_pivot_perc = mean(scale_pivot_perc),
-            scale_Carryover = seq_range(scale_Carryover, n=200),
-            scale_sw_wr = mean(scale_sw_wr),
-            scale_gw_wr = mean(scale_gw_wr)) 
+            scale_Carryover = seq_range(scale_Carryover, n=200))
+            #scale_sw_wr = mean(scale_sw_wr),
+            #scale_gw_wr = mean(scale_gw_wr)) 
 simdata$Name <- NA
 
 epreddraws <-  add_epred_draws(arma_ng, 
@@ -328,108 +328,108 @@ change_Carryover <- epreddraws%>%
 mean(change_Carryover$diff_pred, na.rm = T)
 
 ## SW WATER RIGHT EFFECT ####
-simdata = rf %>%
-  data_grid(scale_class1_urban = mean(scale_class1_urban),
-            scale_irrig_prcp = mean(scale_irrig_prcp),
-            scale_irrig_temp = mean(scale_irrig_temp),
-            scale_et = mean(scale_et),
-            scale_DivFlow = mean(scale_DivFlow),
-            scale_ubrb_prcp = mean(scale_ubrb_prcp),
-            scale_pivot_perc = mean(scale_pivot_perc),
-            scale_Carryover = mean(scale_Carryover),
-            scale_sw_wr = seq_range(scale_sw_wr, n=200),
-            scale_gw_wr = mean(scale_gw_wr))
-simdata$Name <- NA
-
-epreddraws <-  add_epred_draws(arma_ng,
-                               newdata=simdata,
-                               ndraws=1000,
-                               re_formula=NA
-)
-epreddraws$unscale.sw_wr <- (unscale(epreddraws$scale_sw_wr,
-                                         rf$sw_wr))
-
-
-sw_wr <- ggplot(data=epreddraws,
-                    aes(x = unscale.sw_wr, y = exp(.epred))) +
-  stat_lineribbon(
-    .width = c(.5, 0.95), alpha = 0.35, fill="grey",
-    color="black", size=2) +
-  ylab("Drain Discharge (Acre-ft/yr)") + xlab("SW Water Rights")  +
-  theme_bw() +
-  theme(text = element_text(size = 13)) +
-  scale_y_continuous(labels = scales::comma)+
-  coord_cartesian(ylim = c(1000, 40000))
-sw_wr
-ggsave('/Users/dbeisel/Desktop/DATA/Bridget/Drains_Lower_Boise_River/model_output/Figures/sw_wr_marg.jpg',
-       width = 4,
-       height = 4,
-       units = 'in')
-
-change_sw_wr <- epreddraws%>%
-  select(unscale.sw_wr, .epred) %>%
-  group_by(unscale.sw_wr) %>%
-  summarize(avg = mean(exp(.epred))) %>%
-  mutate(diff_pred = c(NA, NA, NA, NA, NA, NA, NA, NA, NA , NA, NA,
-                       NA, NA, NA, NA, NA, NA, NA, NA, NA , NA, NA,
-                       diff(avg, lag = 22)),
-         diff_sw_wr = c(NA, NA, NA, NA, NA, NA, NA, NA, NA , NA, NA,
-                       NA, NA, NA, NA, NA, NA, NA, NA, NA , NA, NA,
-                       diff(unscale.sw_wr, lag = 22)))
-
-mean(change_sw_wr$diff_pred, na.rm = T)
-
-## GW WATER RIGHT EFFECT ####
-simdata = rf %>%
-  data_grid(scale_class1_urban = mean(scale_class1_urban),
-            scale_irrig_prcp = mean(scale_irrig_prcp),
-            scale_irrig_temp = mean(scale_irrig_temp),
-            scale_et = mean(scale_et),
-            scale_DivFlow = mean(scale_DivFlow),
-            scale_ubrb_prcp = mean(scale_ubrb_prcp),
-            scale_pivot_perc = mean(scale_pivot_perc),
-            scale_Carryover = mean(scale_Carryover),
-            scale_sw_wr = mean(scale_sw_wr),
-            scale_gw_wr = seq_range(scale_gw_wr, n=200))
-simdata$Name <- NA
-
-epreddraws <-  add_epred_draws(arma_ng,
-                               newdata=simdata,
-                               ndraws=1000,
-                               re_formula=NA
-)
-epreddraws$unscale.gw_wr <- (unscale(epreddraws$scale_gw_wr,
-                                     rf$gw_wr))
-
-
-gw_wr <- ggplot(data=epreddraws,
-                aes(x = unscale.gw_wr, y = exp(.epred))) +
-  stat_lineribbon(
-    .width = c(.5, 0.95), alpha = 0.35, fill="grey",
-    color="black", size=2) +
-  ylab("Drain Discharge (Acre-ft/yr)") + xlab("GW Water Rights")  +
-  theme_bw() +
-  theme(text = element_text(size = 13)) +
-  scale_y_continuous(labels = scales::comma)+
-  coord_cartesian(ylim = c(1000, 40000))
-gw_wr
-ggsave('/Users/dbeisel/Desktop/DATA/Bridget/Drains_Lower_Boise_River/model_output/Figures/gw_wr_marg.jpg',
-       width = 4,
-       height = 4,
-       units = 'in')
-
-change_gw_wr <- epreddraws%>%
-  select(unscale.gw_wr, .epred) %>%
-  group_by(unscale.gw_wr) %>%
-  summarize(avg = mean(exp(.epred))) %>%
-  mutate(diff_pred = c(NA, NA, NA, NA, NA, NA, NA, NA, NA , NA, NA,
-                       NA, NA, NA, NA, NA, NA, NA, NA, NA , NA, NA,
-                       diff(avg, lag = 22)),
-         diff_gw_wr = c(NA, NA, NA, NA, NA, NA, NA, NA, NA , NA, NA,
-                       NA, NA, NA, NA, NA, NA, NA, NA, NA , NA, NA,
-                       diff(unscale.gw_wr, lag = 22)))
-
-mean(change_gw_wr$diff_pred, na.rm = T)
+# simdata = rf %>%
+#   data_grid(scale_class1_urban = mean(scale_class1_urban),
+#             scale_irrig_prcp = mean(scale_irrig_prcp),
+#             scale_irrig_temp = mean(scale_irrig_temp),
+#             scale_et = mean(scale_et),
+#             scale_DivFlow = mean(scale_DivFlow),
+#             scale_ubrb_prcp = mean(scale_ubrb_prcp),
+#             scale_pivot_perc = mean(scale_pivot_perc),
+#             scale_Carryover = mean(scale_Carryover),
+#             scale_sw_wr = seq_range(scale_sw_wr, n=200),
+#             scale_gw_wr = mean(scale_gw_wr))
+# simdata$Name <- NA
+# 
+# epreddraws <-  add_epred_draws(arma_ng,
+#                                newdata=simdata,
+#                                ndraws=1000,
+#                                re_formula=NA
+# )
+# epreddraws$unscale.sw_wr <- (unscale(epreddraws$scale_sw_wr,
+#                                          rf$sw_wr))
+# 
+# 
+# sw_wr <- ggplot(data=epreddraws,
+#                     aes(x = unscale.sw_wr, y = exp(.epred))) +
+#   stat_lineribbon(
+#     .width = c(.5, 0.95), alpha = 0.35, fill="grey",
+#     color="black", size=2) +
+#   ylab("Drain Discharge (Acre-ft/yr)") + xlab("SW Water Rights")  +
+#   theme_bw() +
+#   theme(text = element_text(size = 13)) +
+#   scale_y_continuous(labels = scales::comma)+
+#   coord_cartesian(ylim = c(1000, 40000))
+# sw_wr
+# ggsave('/Users/dbeisel/Desktop/DATA/Bridget/Drains_Lower_Boise_River/model_output/Figures/sw_wr_marg.jpg',
+#        width = 4,
+#        height = 4,
+#        units = 'in')
+# 
+# change_sw_wr <- epreddraws%>%
+#   select(unscale.sw_wr, .epred) %>%
+#   group_by(unscale.sw_wr) %>%
+#   summarize(avg = mean(exp(.epred))) %>%
+#   mutate(diff_pred = c(NA, NA, NA, NA, NA, NA, NA, NA, NA , NA, NA,
+#                        NA, NA, NA, NA, NA, NA, NA, NA, NA , NA, NA,
+#                        diff(avg, lag = 22)),
+#          diff_sw_wr = c(NA, NA, NA, NA, NA, NA, NA, NA, NA , NA, NA,
+#                        NA, NA, NA, NA, NA, NA, NA, NA, NA , NA, NA,
+#                        diff(unscale.sw_wr, lag = 22)))
+# 
+# mean(change_sw_wr$diff_pred, na.rm = T)
+# 
+# ## GW WATER RIGHT EFFECT ####
+# simdata = rf %>%
+#   data_grid(scale_class1_urban = mean(scale_class1_urban),
+#             scale_irrig_prcp = mean(scale_irrig_prcp),
+#             scale_irrig_temp = mean(scale_irrig_temp),
+#             scale_et = mean(scale_et),
+#             scale_DivFlow = mean(scale_DivFlow),
+#             scale_ubrb_prcp = mean(scale_ubrb_prcp),
+#             scale_pivot_perc = mean(scale_pivot_perc),
+#             scale_Carryover = mean(scale_Carryover),
+#             scale_sw_wr = mean(scale_sw_wr),
+#             scale_gw_wr = seq_range(scale_gw_wr, n=200))
+# simdata$Name <- NA
+# 
+# epreddraws <-  add_epred_draws(arma_ng,
+#                                newdata=simdata,
+#                                ndraws=1000,
+#                                re_formula=NA
+# )
+# epreddraws$unscale.gw_wr <- (unscale(epreddraws$scale_gw_wr,
+#                                      rf$gw_wr))
+# 
+# 
+# gw_wr <- ggplot(data=epreddraws,
+#                 aes(x = unscale.gw_wr, y = exp(.epred))) +
+#   stat_lineribbon(
+#     .width = c(.5, 0.95), alpha = 0.35, fill="grey",
+#     color="black", size=2) +
+#   ylab("Drain Discharge (Acre-ft/yr)") + xlab("GW Water Rights")  +
+#   theme_bw() +
+#   theme(text = element_text(size = 13)) +
+#   scale_y_continuous(labels = scales::comma)+
+#   coord_cartesian(ylim = c(1000, 40000))
+# gw_wr
+# ggsave('/Users/dbeisel/Desktop/DATA/Bridget/Drains_Lower_Boise_River/model_output/Figures/gw_wr_marg.jpg',
+#        width = 4,
+#        height = 4,
+#        units = 'in')
+# 
+# change_gw_wr <- epreddraws%>%
+#   select(unscale.gw_wr, .epred) %>%
+#   group_by(unscale.gw_wr) %>%
+#   summarize(avg = mean(exp(.epred))) %>%
+#   mutate(diff_pred = c(NA, NA, NA, NA, NA, NA, NA, NA, NA , NA, NA,
+#                        NA, NA, NA, NA, NA, NA, NA, NA, NA , NA, NA,
+#                        diff(avg, lag = 22)),
+#          diff_gw_wr = c(NA, NA, NA, NA, NA, NA, NA, NA, NA , NA, NA,
+#                        NA, NA, NA, NA, NA, NA, NA, NA, NA , NA, NA,
+#                        diff(unscale.gw_wr, lag = 22)))
+# 
+# mean(change_gw_wr$diff_pred, na.rm = T)
 
 
 ## UBRB PRECIP EFFECT ####
@@ -442,9 +442,9 @@ simdata = rf %>%
             scale_DivFlow = mean(scale_DivFlow),
             scale_ubrb_prcp = seq_range(scale_ubrb_prcp, n=200),
             scale_pivot_perc = mean(scale_pivot_perc),
-            scale_Carryover = mean(scale_Carryover),
-            scale_sw_wr = mean(scale_sw_wr),
-            scale_gw_wr = mean(scale_gw_wr)) 
+            scale_Carryover = mean(scale_Carryover))
+            #scale_sw_wr = mean(scale_sw_wr),
+            #scale_gw_wr = mean(scale_gw_wr)) 
 simdata$Name <- NA
 
 epreddraws <-  add_epred_draws(arma_ng, 
@@ -492,12 +492,12 @@ simdata = rf %>%
             scale_irrig_prcp = seq_range(scale_irrig_prcp, n=200),
             scale_irrig_temp = mean(scale_irrig_temp),
             scale_et = mean(scale_et),
-            scale_DivFlow = mean(scale_DivFlow),
+            scale_DivFlow = mean(scale_DivFlow))
             scale_ubrb_prcp = mean(scale_ubrb_prcp),
             scale_pivot_perc = mean(scale_pivot_perc),
             scale_Carryover = mean(scale_Carryover),
-            scale_sw_wr = mean(scale_sw_wr),
-            scale_gw_wr = mean(scale_gw_wr)) 
+            #scale_sw_wr = mean(scale_sw_wr),
+            #scale_gw_wr = mean(scale_gw_wr)) 
 simdata$Name <- NA
 
 epreddraws <-  add_epred_draws(arma_ng, 
@@ -545,12 +545,12 @@ simdata = rf %>%
             scale_irrig_prcp = mean(scale_irrig_prcp),
             scale_irrig_temp = mean(scale_irrig_temp),
             scale_et = mean(scale_et),
-            scale_DivFlow = seq_range(scale_DivFlow, n=200),
+            scale_DivFlow = seq_range(scale_DivFlow, n=200))
             scale_ubrb_prcp = mean(scale_ubrb_prcp),
             scale_pivot_perc = mean(scale_pivot_perc),
             scale_Carryover = mean(scale_Carryover),
-            scale_sw_wr = mean(scale_sw_wr),
-            scale_gw_wr = mean(scale_gw_wr)) 
+            #scale_sw_wr = mean(scale_sw_wr),
+            #scale_gw_wr = mean(scale_gw_wr)) 
 simdata$Name <- NA
 
 epreddraws <-  add_epred_draws(arma_ng, 
@@ -821,14 +821,15 @@ mcmc_plot(arma_ng,
           type = 'areas',
           variable = c('b_scale_et',
                        'b_scale_irrig_prcp', 
-                       'b_scale_irrig_temp',
-                       'b_scale_ubrb_prcp'),
+                       'b_scale_irrig_temp'),
+                      # 'b_scale_ubrb_prcp'),
           prob = 0.95) +
   theme_bw() +
   vline_0() +
   scale_y_discrete(labels = c('Evapotranspiration',
                               'Drainshed Irrigation Season Precipitation',
-                              'Temperature', 'UBRB Water Year Precip')) +
+                              'Temperature')) + 
+                              #'UBRB Water Year Precip')) +
   xlab('Relative Effect Size (log)') +
   theme(text = element_text(size=15, family = 'Arial'))
 ggsave('/Users/dbeisel/Desktop/DATA/Bridget/Drains_Lower_Boise_River/model_output/Figures/postmass_climate.jpg', 
@@ -838,7 +839,7 @@ ggsave('/Users/dbeisel/Desktop/DATA/Bridget/Drains_Lower_Boise_River/model_outpu
 
 # All
 
-color_scheme_set('blue')
+#color_scheme_set('blue')
 mcmc_plot(arma_ng,
           type = 'areas',
           variable = c('b_scale_et',
@@ -848,9 +849,9 @@ mcmc_plot(arma_ng,
                        'b_scale_DivFlow',
                        'b_scale_Carryover',
                        'b_scale_class1_urban',
-                       'b_scale_pivot_perc',
-                       'b_scale_sw_wr',
-                       'b_scale_gw_wr'),
+                       'b_scale_pivot_perc'),
+                      # 'b_scale_sw_wr',
+                      # 'b_scale_gw_wr'),
           prob = 0.95) +
   theme_bw() +
   vline_0() +
@@ -861,9 +862,9 @@ mcmc_plot(arma_ng,
                               'Canal Flows',
                               'Reservoir Carryover',
                               'Urban Percentage',
-                              'Pivot Irrigation Percentage',
-                              'SW Water Rights',
-                              'GW Water Rights')) +
+                              'Pivot Irrigation Percentage')) +
+                             # 'SW Water Rights',
+                            # 'GW Water Rights')) +
   scale_fill_manual(values = c('Evapotranspiration' = '#00798c',
                                'Irrigation Season Precipitation' = 'grey',
                                'Temperature' = '#DF2935',
@@ -871,12 +872,13 @@ mcmc_plot(arma_ng,
                                'Canal Flows' = '#00798c',
                                'Reservoir Carryover' = '#00798c',
                                'Urban Percentage' = '#DF2935',
-                               'Pivot Irrigation Percentage' = '#DF2935',
-                               'SW Water Rights' = 'grey',
-                               'GW Water Rights' = 'grey'))+
+                               'Pivot Irrigation Percentage' = '#DF2935')) +
+                              # 'SW Water Rights' = 'grey',
+                              # 'GW Water Rights' = 'grey'))+
   xlab('Relative Effect Size (log)') +
-  theme(text = element_text(size=15, family = 'Arial'))
-ggsave('/Users/dbeisel/Desktop/DATA/Bridget/Drains_Lower_Boise_River/model_output/Figures/postmass_all.png', 
+  theme(text = element_text(size=15, family = 'Arial'),
+        plot.margin = unit(c(0.1, 0.1, 0.1, 0.1), "in"))  # Adjust margins to reduce spacing
+ggsave('/Users/dbeisel/Desktop/DATA/Bridget/Drains_Lower_Boise_River/model_output/Figures/postmass_all.svg', 
        width = 4.5,
        height = 4.5,
        units = 'in')
@@ -933,7 +935,7 @@ ggsave('/Users/dbeisel/Desktop/DATA/Bridget/Drains_Lower_Boise_River/model_outpu
 
 ## Marginal effects in on plot
 
-ggarrange(sw_wr, gw_wr, et, temp, precip, ubrb_prcp, Carryover, canal, pivot, urban, ncol=2, nrow = 5, labels = c('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'))
+ggarrange(et, temp, precip, ubrb_prcp, Carryover, canal, pivot, urban, ncol=2, nrow = 4, labels = c('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H'))
 ggsave('/Users/dbeisel/Desktop/DATA/Bridget/Drains_Lower_Boise_River/model_output/Figures/combined_marg.jpg', 
        width = 4.5,
        height = 4.5,
